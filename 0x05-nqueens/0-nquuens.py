b51e1ac5d89bdcd4c705b4f1f76af6aff8f07693
Cheserem Titus <cheseremtitus24@gmail.com>
2023-08-22 12:51:48 +0300

    modified:   0-nquuens.py

--- 0x05-nqueens/0-nquuens.py
+++ 0x05-nqueens/0-nquuens.py
@@ -1,20 +1,47 @@
 #!/usr/bin/python3
+""" N queens """
 import sys
 
-print("This is the name of the script:", sys.argv[0])
-if len(sys.argv) < 2:
+
+if len(sys.argv) != 2:
     print("Usage: nqueens N")
     exit(1)
-else:
-    N = sys.argv[1]
 
-    if not N.isnumeric():
-        print("N must be a number")
-        exit(1)
+n = sys.argv[1]
+
+if not n.isdigit():
+    print("N must be a number")
+    exit(1)
+
+n = int(n)
+
+if n < 4:
+    print("N must be at least 4")
+    exit(1)
+
+
+def pos_queens(n, num=0, first=[], sec=[], third=[]):
+    """ find possible positions """
+    if num < n:
+        for i in range(n):
+            if i not in first and num + i not in sec and num - i not in third:
+                yield from pos_queens(n, num + 1, first + [i],
+                                      sec + [num + i], third + [num - i])
     else:
-        if N < 4:
-            print("N must be at least 4")
-            exit(1)
-        else:
-            print("Number of arguments:", len(sys.argv))
-            print("The arguments are:" , str(sys.argv))
+        yield first
+
+
+def sol(n):
+    """ solve """
+    fin = []
+    i = 0
+    for ans in pos_queens(n, 0):
+        for a in ans:
+            fin.append([i, a])
+            i += 1
+        print(fin)
+        fin = []
+        i = 0
+
+
+sol(n)
